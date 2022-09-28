c625e5b97c77f00e878568acb5aa005a41a96641
danielcol <danielcollins90@gmail.com>
2022-09-28 11:10:06 +0100

    280922 - DPC - v2.0

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 # This is my simple script to grab the manifest from a BlackVue dash cam and then grab the files.
 # DPC 220922
+# DPC 280922 - Added looped checking for liveliness, improved manifest to create summary list
 
 # Imports
 import os
 import shutil
 import urllib3
 import logging
-from ping3 import ping
+import platform
+import subprocess
+import time
 from datetime import datetime
 from urllib3.util import Retry
 from urllib3.util import Timeout
@@ -18,9 +21,14 @@ blackvueHost = '10.100.0.121'
 blackvueBase = 'http://' + blackvueHost + '/'
 blackvueVOD = 'blackvue_vod.cgi'
 recordingFolder = 'Recordings'
-runfilesdir = 'run_files'
 pidfile = 'pidfile.pid'
 logfile = 'blackvuegrab.log'
+pid = os.getpid()
+pingspacer = 5
+attempts = 0
+innerattempts = 0
+loopcounter = 0
+timeoutspacer = 500
 workingmanifest = []
 manifest = []
 
@@ -51,27 +59,20 @@ def CreateFilePath(foldervar, filename):
     CreateFilePathStep3 = os.path.join(CreateFilePathStep1, ConvDate, CreateFilePathStep2)
     return CreateFilePathStep3
 
-expLogFile = CreatePath(runfilesdir, logfile)
-expPidFile = CreatePath(runfilesdir, pidfile)
-
 # Log defs
 
-logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename=expLogFile, level=logging.INFO)
-logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename=expLogFile, level=logging.DEBUG)
-logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename=expLogFile, level=logging.ERROR)
-logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename=expLogFile, level=logging.WARNING)
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename=logfile, level=logging.INFO)
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename=logfile, level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename=logfile, level=logging.ERROR)
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename=logfile, level=logging.WARNING)
 
 # We need to store the manifest of the files, as the server is slow and unrelliable
 
-def IsNo5Alive(host):
-    resp = ping(host)
-    LogFunc("Checking if host {} is alive".format(host), 'info')
-    if resp == False:
-        LogFunc("{} is dead jim. Exit.".format(host), 'error')
-        return False
-    else:
-        LogFunc("{} is alive! continuing".format(host), 'info')
-        return True
+def PingTest(host, timeout, count):
+    param1 = '-n' if platform.system().lower() == 'windows' else '-c'
+    param2 = '-w' if platform.system().lower() == 'windows' else '-W'
+    command = ['ping', param1, str(count), param2, str(timeout), host]
+    return subprocess.call(command, stdout=subprocess.DEVNULL) == 0
 
 def GetManifest():
     global workingmanifest
@@ -88,42 +89,55 @@ def GetManifest():
         LogFunc("Couldn't get the manifest from {}".format(url), 'error')
         LogFunc("Failed due to {}".format(m_err.reason), 'error')
 
-def ManifestToNiceList():
-    global workingmanifest, manifest
-    string = workingmanifest
+def ManifestToNiceListv2():
+    global manifest
+    step4 = []
     LogFunc("working on the manifest..", 'info')
-    step1 = list(string.split(","))
+    step1 = list(workingmanifest.split(","))
     step2 = [elem.replace('v:1.00\r\nn:', '') for elem in step1]
     step3 = [elem.replace('s:1000000\r\nn:', '') for elem in step2]
     step3.pop()
-    manifest = step3
-    LogFunc("manifest tidy up complete", 'info')
+    LogFunc("The base manifest has {} items in it".format(len(step3)), 'info')
+    for file in step3:
+        curfile = os.path.basename(file)
+        curfilepath = CreateFilePath(recordingFolder, curfile)
+        if not os.path.isfile(curfilepath):
+            step4.append(file)
+        else:
+            pass
+    LogFunc("Manifest tidy up complete", 'info')
+    LogFunc("The trimmed manifest has {} items to work on".format(len(step4)), 'info')
+    manifest = step4
 
 def GetFilesFromBlackVue():
-    global manifest
+    global manifest, loopcounter
     retry = Retry(10, status=10, connect=30, read=30, other=30, backoff_factor=2)
     timeout = Timeout(connect=10, read=30)
     http = urllib3.PoolManager(timeout=timeout, retries=retry)
     LogFunc("Starting grab loop of files", 'info')
-    counter = 0
     for file in manifest:
         curfile = os.path.basename(file)
         LogFunc("Loop start for {}".format(curfile), 'info')
         LogFunc("Calling Directory Checker for {}".format(curfile), 'info')
-        CreatePathFolder(recordingFolder, curfile)
         curfilepath = CreateFilePath(recordingFolder, curfile)
         if not os.path.isfile(curfilepath):
             currenturl = blackvueBase + file
             LogFunc("Downloading..{}".format(currenturl), 'info')
-            geturldata = http.urlopen('GET', currenturl, preload_content=False)
-            tmpfile = open(curfilepath, 'wb')
-            shutil.copyfileobj(geturldata, tmpfile)
-            LogFunc("Download of {} done!".format(currenturl), 'info')
-            addcounter = counter + 1
+            try:
+                geturldata = http.urlopen('GET', currenturl, preload_content=False)
+                tmpfile = open(curfilepath, 'wb')
+                shutil.copyfileobj(geturldata, tmpfile)
+                LogFunc("Download of {} done!".format(currenturl), 'info')
+            except Exception as e:
+                LogFunc("Uh-oh there's an issue {}".format(e), 'error')
+                LogFunc("Calling rigorous testing..", 'error')
+                RigorousTesting()
+            loopcounter = loopcounter + 1
+        elif os.path.isfile(curfilepath):
+            LogFunc("File {} exists.. not downloading".format(curfilepath), 'info')
         else:
-            LogFunc("Failed after {} files".format(counter), 'error')
-            LogFunc("{} exists".format(curfile), 'error')
-    LogFunc("Successfully downloaded {} files".format(counter), 'info')
+            LogFunc("Failed after {} files".format(loopcounter), 'error')
+    LogFunc("Successfully downloaded {} files".format(loopcounter), 'info')
     LogFunc("I'm finished here! see ya later..", 'info')
 
 def LogFunc(messagetolog, level):
@@ -137,16 +151,82 @@ def LogFunc(messagetolog, level):
         logging.warning("Not sure? Wrong log level!")
         logging.warning(messagetolog)
 
-def RunTheProg():
-    for i in range (5):
-        try:
-            if IsNo5Alive(blackvueHost) == True:
-                LogFunc("Successful host check after {} attempts. Continuing".format(i), 'info')
-                GetManifest()
-                ManifestToNiceList()
-                GetFilesFromBlackVue()
-        finally:
-            LogFunc("Host check failed after {} attempts. Exiting".format(i), 'error')
-            break
+def LogFuncBreak(opt):
+    if opt == 'start':
+        LogFunc("", 'info')
+        LogFunc("************** SCRIPT START **************", 'info')
+        LogFunc("*                                        *", 'info')
+        LogFunc("******************************************", 'info')
+        LogFunc("", 'info')
+    elif opt == 'end':
+        LogFunc("", 'info')
+        LogFunc("**************  SCRIPT END  **************", 'info')
+        LogFunc("*                                        *", 'info')
+        LogFunc("******************************************", 'info')
+        LogFunc("", 'info')
+    elif opt == 'bad':
+        LogFunc("", 'error')
+        LogFunc("**************  ERROR! END  **************", 'error')
+        LogFunc("*                                        *", 'error')
+        LogFunc("******************************************", 'error')
+        LogFunc("", 'error')
+    elif opt == 'badtest':
+        LogFunc("", 'error')
+        LogFunc("*********** INITIAL TEST FAILED **********", 'error')
+        LogFunc("", 'error')
 
-RunTheProg()
+def ProgLoop():
+    global innerattempts
+    if innerattempts == 5:
+        LogFunc("Exhausted {} attempts, exiting.".format(innerattempts), 'error')
+        LogFunc("Deleting PID {}".format(pid), 'info')
+        LogFuncBreak('bad')
+        os.remove(pidfile)
+    elif PingTest(blackvueHost, '100', '10') == False:
+        LogFuncBreak('badtest')
+        RigorousTesting()
+    elif PingTest(blackvueHost, '100', '5') == True:
+        LogFuncBreak('start')
+        LogFunc("Successful host check - continuing", 'info')
+        MainLoop()
+        LogFunc("Deleting PID {}".format(pid), 'info')
+        LogFuncBreak('end')
+        os.remove(pidfile)
+
+def RigorousTesting():
+    global pingspacer, attempts, innerattempts, timeoutspacer
+    LogFunc("Launching rigorous testing", 'info')
+    LogFunc("Testing has been called {} times".format(attempts), 'info')
+    LogFunc("Attempt number {}...".format(innerattempts), 'info')
+    attempts = attempts +1
+    if PingTest(blackvueHost, timeoutspacer, '5') == True:
+        LogFunc("...alive again - never mind!", 'info')
+        ProgLoop()
+    elif PingTest(blackvueHost, '500', '5') == False:
+        LogFunc("Initial test failed - launching loop test with back-off", 'error')
+        timeoutspacer = timeoutspacer + 100
+        LogFunc("Sleeping for {} seconds before next test...".format(pingspacer), 'info')
+        time.sleep(pingspacer)
+        pingspacer = pingspacer + 5
+        innerattempts = innerattempts + 1
+        ProgLoop()
+
+def PidCheck():
+    if os.path.exists(pidfile):
+        LogFunc("a PID file exists! Exiting", 'error')
+        LogFuncBreak('bad')
+        return True
+    elif not os.path.exists(pidfile):
+        with open(pidfile, 'w') as out:
+            out.write(str(pid))
+        LogFunc("Writing PID {} to file".format(pid), 'info')
+        return False
+def MainLoop():
+    LogFunc("MainLoop Start", 'info')
+    GetManifest()
+    ManifestToNiceListv2()
+    GetFilesFromBlackVue()
+    LogFunc("MainLoop end", 'info')
+
+if PidCheck() == False:
+    ProgLoop()
--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
+280922 - DPC - v2.0
+
+- Added testing functionality for liveliness.. 
+- Tidied the manifest up so only a trimmed list is provided for the loop
+- Added pid checking at script start
+
 240922 - DPC - BlackVue grab script.
 
 Written to reach out to my BlackVue dash cam, pull the files down and sort into folders.
